d4da2f28f9c14cd90b07f88126624582827e99df
Michał Bisikiewicz <64103517+michalBisikiewicz@users.noreply.github.com>
2022-01-17 18:14:01 +0100

    Activated user login URL for the API View

--- DRF/settings.py
+++ DRF/settings.py
@@ -128,6 +128,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ]
 }
+# Permissions:
+# AllowAny
+# IsAutenticated
+# IsAdminUser
+# IsAuthenticatedOrReadOnly
