dc3c0cf828d9122c84df3d36ba4953198d08fcba
Michał Bisikiewicz <64103517+michalBisikiewicz@users.noreply.github.com>
2022-01-30 22:23:56 +0100

    Built a custom user model

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,15 @@
 from django.db import models
-from django.contrib.auth.models import User
 from django.utils import timezone
+from django.conf import settings
 
 # Create your models here.
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = "categories"
 
     def __str__(self):
         return self.name
@@ -28,7 +31,7 @@ class Post(models.Model):
     content = models.TextField(max_length=1000, default='')
     slug = models.SlugField(max_length=250, unique_for_date='published')
     published = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='blog_posts')
     status = models.CharField(max_length=10, choices=options, default='published')
     objects = models.Manager()   # default manager
     postobjects = PostObjects()     # custom manager
